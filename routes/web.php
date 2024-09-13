c165c1c919ee7dc991a1d54dba75dd8eab580c28
CaiooSouzaa <caioggsouza@hotmail.com>
2024-09-12 23:35:43 -0300

    new commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout_inicial.index');
 });
+
+Route::get('/barbers', function () {
+    return view('barbers');
+});
